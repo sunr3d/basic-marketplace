8abf3da7e8f79954da7dcfae0c36ed18edc1514c
Sunred <sunr3d.coding@gmail.com>
2025-07-19 21:11:37 +0300

    chore: регистрация сервиса в контейнер зависимостей

--- internal/bootstrap/container.go
+++ internal/bootstrap/container.go
@@ -9,11 +9,13 @@ import (
 	"github.com/sunr3d/basic-marketplace/internal/config"
 	"github.com/sunr3d/basic-marketplace/internal/infra"
 	"github.com/sunr3d/basic-marketplace/internal/interfaces"
+	"github.com/sunr3d/basic-marketplace/internal/logic"
 )
 
 type Container struct {
 	DB       *gorm.DB
 	UserRepo interfaces.UserRepo
+	UserService interfaces.UserService
 	// ... другие зависимости, добавим позже
 }
 
@@ -23,10 +25,12 @@ func NewContainer(cfg *config.Config, log *zap.Logger) (*Container, error) {
 		return nil, fmt.Errorf("infra.InitPostgres: %w", err)
 	}
 	userRepo := infra.NewUserRepoPG(db)
+	userService := logic.NewUserService(userRepo)
 	// ... еще будут зависимости, типа Редиса для кеширования
 
 	return &Container{
 		DB:       db,
 		UserRepo: userRepo,
+		UserService: userService,
 	}, nil
 }
