4d3bc58a8f70d8983ee4be3b6f5a564de1218f08
Sunred <sunr3d.coding@gmail.com>
2025-07-20 14:43:09 +0300

    feat: перехватчик авторизации (AuthMiddleware)

---
+++ internal/handlers/middleware.go
@@ -0,0 +1,32 @@
+package handlers
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func AuthMiddleware(jwtSecret []byte) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "требуется авторизация"})
+			return
+		}
+		tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
+		claims := jwt.MapClaims{}
+		token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (any, error) {
+			return jwtSecret, nil
+		})
+		if err != nil || !token.Valid {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "некорректный токен"})
+			return
+		}
+
+		c.Set("user_id", claims["user_id"])
+		c.Set("login", claims["login"])
+		c.Next()
+	}
+}
